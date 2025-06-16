bd0edff185ed7a2fa02ae6b2edef63dd5329c99d
Andrew Hart <ahart@cern.ch>
2025-06-16 13:03:13 -0400

    Temporary changes for testing.

--- IntegrationTests/common/hdl/tf_mem.vhd
+++ IntegrationTests/common/hdl/tf_mem.vhd
@@ -122,47 +122,30 @@ process(clka)
   variable overwrite    : std_logic := '1';
 begin
   if rising_edge(clka) then -- ######################################### Start counter initially
-    if DEBUG then
-      if (NUM_PAGES = 2) then
-        report "tf_mem "&NAME&" nent(0) nent(1) "&to_bstring(nent_o(0))&" "&to_bstring(nent_o(1));
-      end if;
-      if (NUM_PAGES = 8) then
-        report "tf_mem "&NAME&" nent(0)...nent(7) "&to_bstring(nent_o(0))&" "&to_bstring(nent_o(1))&" "&to_bstring(nent_o(2))&" "&to_bstring(nent_o(3))&" "&to_bstring(nent_o(4))&" "&to_bstring(nent_o(5))&" "&to_bstring(nent_o(6))&" "&to_bstring(nent_o(7));
-      end if;
-    end if;
     slv_page_cnt_save := slv_page_cnt;
     if (init = '0' and to_integer(unsigned(slv_clk_cnt)) < MAX_ENTRIES-1) then -- ####### Counter nent
       slv_clk_cnt := std_logic_vector(unsigned(slv_clk_cnt)+1);     
-      --report time'image(now)&" tf_mem "&NAME&" increment vi_clk_cnt:"&integer'image(vi_clk_cnt);
     elsif (to_integer(unsigned(slv_clk_cnt)) >= MAX_ENTRIES-1) then -- -1 not included
-      --report time'image(now)&" tf_mem "&NAME&" goto next page";
       slv_clk_cnt := (others => '0');
-      --assert (vi_page_cnt < NUM_PAGES) report "vi_page_cnt out of range" severity error;
       if (to_integer(unsigned(slv_page_cnt)) < NUM_PAGES-1) then -- Assuming linear continuous page access
         slv_page_cnt := std_logic_vector(unsigned(slv_page_cnt)+1);
-        --report time'image(now)&" tf_mem "&NAME&" increment vi_page_cnt:"&integer'image(vi_page_cnt);
       else
-        --report time'image(now)&" tf_mem "&NAME&" resetting vi_page_cnt";
          slv_page_cnt := (others => '0');
       end if;
-      --report time'image(now)&" tf_mem "&NAME&" will zero nent";
       nent_o(to_integer(unsigned(slv_page_cnt))) <= (others => '0');
     end if;
     if (sync_nent='1') and (init='1') then
-      --report time'image(now)&" tf_mem "&NAME&" sync_nent";
       init := '0';
       slv_clk_cnt := (others => '0');
       slv_page_cnt := (0 => '1', others => '0');
     end if;
     if (wea='1') then
       overwrite := addra(0);
-      --vi_page_cnt_slv := std_logic_vector(to_unsigned(vi_page_cnt_save,vi_page_cnt_slv'length));
       if (overwrite = '0') then
         address := slv_page_cnt_save&nent_o(to_integer(unsigned(slv_page_cnt_save)));
       else
         address := slv_page_cnt_save&std_logic_vector(to_unsigned(to_integer(unsigned(nent_o(to_integer(unsigned(slv_page_cnt_save)))))-1,nent_o(to_integer(unsigned(slv_page_cnt_save)))'length));
       end if;
-      --report "tf_mem "&time'image(now)&" "&NAME&" page writeaddr "&" "&to_bstring(slv_page_cnt_save)&" "&to_bstring(address)&" "&to_bstring(overwrite)&" "&to_bstring(dina)&" addra "&to_bstring(addra);
       sa_RAM_data(to_integer(unsigned(address))) <= dina; -- Write data
       if (overwrite = '0') then
         nent_o(to_integer(unsigned(slv_page_cnt_save))) <= std_logic_vector(to_unsigned(to_integer(unsigned(nent_o(to_integer(unsigned(slv_page_cnt_save))))) + 1, nent_o(to_integer(unsigned(slv_page_cnt_save)))'length)); -- + 1 (slv)
@@ -175,9 +158,6 @@ process(clkb)
 begin
   if rising_edge(clkb) then
     if (enb='1') then
-      if DEBUG then
-        report "tf_mem "&time'image(now)&" "&NAME&" readaddr "&to_bstring(addrb)&" "&to_bstring(sa_RAM_data(to_integer(unsigned(addrb))));
-      end if;
       sv_RAM_row <= sa_RAM_data(to_integer(unsigned(addrb)));
     end if;
   end if;
